8eb527fb7e05f2e14408efbcebee3927fc7151c8
meafmira <i@meafmira.me>
2022-05-01 18:08:42 +0530

    fix: Link component

--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -3,11 +3,21 @@ export interface LinkProps {
   href?: string
   children: React.ReactNode
   renderLink?: (Component: React.ElementType) => React.ReactNode
+  className?: string
 }
 
-export function Link({ as: Component = "a", children, href }: LinkProps) {
+export function Link({
+  as: Component = "a",
+  children,
+  href,
+  ...props
+}: LinkProps) {
   return (
-    <Component href={href} className="text-blue-500 text-xl font-semibold">
+    <Component
+      href={href}
+      className="text-blue-500 text-xl font-semibold"
+      {...props}
+    >
       {children}
     </Component>
   )
